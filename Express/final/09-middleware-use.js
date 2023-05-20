dadf8658adfd5f8f9238ccf7a357c82577f142a6
Pratham <ppratham812@gmail.com>
2023-05-20 18:14:03 +0530

    made few changes

--- Express/final/09-middleware-use.js
+++ Express/final/09-middleware-use.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
-//  req => middleware => res
-app.use([logger, authorize])
-// api/home/about/products
+const express = require('express');
+const app = express();
+const logger = require('../logger.js')
+
+app.use('/api', logger); // here, /api is the path for which middleware will be invoked.
+// req -> middleware -> res
 app.get('/', (req, res) => {
-  res.send('Home')
+    res.send('Home page');
 })
+
 app.get('/about', (req, res) => {
-  res.send('About')
+    res.send('About page');
 })
+
 app.get('/api/products', (req, res) => {
-  res.send('Products')
+    res.send('Products');
 })
+
 app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
+    res.send('Items');
 })
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+    console.log(`Server is listening on port 5000`);
 })
